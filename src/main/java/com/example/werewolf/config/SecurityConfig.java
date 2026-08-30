127e1410e35521aeb472ea8e6f00b65b6a3490ae
yuzuko.saito <294679403+sayu0104@users.noreply.github.com>
2026-08-30 09:55:53 +0900

    feat: パスワードを暗号化し、管理者ユーザーを定義する（認証Step3）
    closes #53

--- src/main/java/com/example/werewolf/config/SecurityConfig.java
+++ src/main/java/com/example/werewolf/config/SecurityConfig.java
@@ -3,10 +3,30 @@ package com.example.werewolf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // 「これは設定ファイルですよ」の目印
 public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails admin = User.withUsername("admin")
+				.password("$2a$10$2kBLUxo.iAFiwpEGMjmnC.LbnFCPiqMhTtX5mlWnCvR.Py9GaoYVy")
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(admin);
+	}
 
 	@Bean // 「この部品を、Springに登録して使わせる」目印
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
